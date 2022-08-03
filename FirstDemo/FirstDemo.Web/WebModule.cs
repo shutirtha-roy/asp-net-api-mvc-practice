262d25c516d49441be7596d3c75ee748162254ac
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-08-04 00:03:19 +0100

    WebModule Added for Autofac

---
+++ FirstDemo/FirstDemo.Web/WebModule.cs
@@ -0,0 +1,19 @@
+﻿using Autofac;
+using FirstDemo.Web.Models;
+
+namespace FirstDemo.Web
+{
+    public class WebModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<CourseModel>().As<ICourseModel>()
+                .InstancePerLifetimeScope();
+
+            builder.RegisterType<CourseModel>().AsSelf();
+
+
+            base.Load(builder);
+        }
+    }
+}
