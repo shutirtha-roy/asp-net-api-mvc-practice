a4e7c95759dc480f381ec478046822a68fd7018a
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-08-31 09:43:30 +0100

    CourseController Added

---
+++ FirstDemo/FirstDemo.Web/Areas/Admin/Controllers/CourseController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FirstDemo.Web.Areas.Admin.Controllers
+{
+    public class CourseController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
