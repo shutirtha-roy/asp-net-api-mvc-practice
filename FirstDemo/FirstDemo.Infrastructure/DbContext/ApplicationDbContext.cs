03bb83f0afeef0e3f500cc00452985bc101a260b
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-09-01 17:59:13 +0100

    ApplicationDbContext code changed since project is changed

--- FirstDemo/FirstDemo.Infrastructure/DbContext/ApplicationDbContext.cs
+++ FirstDemo/FirstDemo.Infrastructure/DbContext/ApplicationDbContext.cs
@@ -5,9 +5,24 @@ namespace FirstDemo.Infrastructure.DbContexts
 {
     public class ApplicationDbContext : IdentityDbContext
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
+        private readonly string _connectionString;
+        private readonly string _migrationAssemblyName;
+        public ApplicationDbContext(string connectionString, string migrationAssemblyName)
         {
+            _connectionString = connectionString;
+            _migrationAssemblyName = migrationAssemblyName;
         }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(_connectionString,
+                    b => b.MigrationsAssembly(_migrationAssemblyName)
+                );
+            }
+
+            base.OnConfiguring(optionsBuilder);
+        }
+
     }
 }
