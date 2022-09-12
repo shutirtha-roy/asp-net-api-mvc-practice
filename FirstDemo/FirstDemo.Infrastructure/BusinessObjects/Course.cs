bad70fe38c5d3844f185270d6647d8e583cd3ebf
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-09-13 04:39:53 +0600

    Course Business Object Implemented

--- FirstDemo/FirstDemo.Infrastructure/BusinessObjects/Course.cs
+++ FirstDemo/FirstDemo.Infrastructure/BusinessObjects/Course.cs
@@ -8,5 +8,15 @@ namespace FirstDemo.Infrastructure.BusinessObjects
 {
     public class Course
     {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public double Fees { get; set; }
+        public DateTime ClassStartDate { get; set; }
+
+        public void SetProperClassStartDate()
+        {
+            if (ClassStartDate.Subtract(DateTime.Now).TotalDays < 7)
+                ClassStartDate = DateTime.Now.AddDays(30);
+        }
     }
 }
