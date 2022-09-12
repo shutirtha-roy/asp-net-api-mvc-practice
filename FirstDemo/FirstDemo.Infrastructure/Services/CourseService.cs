8028b99072fcd0fbed52d6f15f1dbcddbacbd6a7
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-09-13 04:46:00 +0600

    CreateCourse Method Added

--- FirstDemo/FirstDemo.Infrastructure/Services/CourseService.cs
+++ FirstDemo/FirstDemo.Infrastructure/Services/CourseService.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CourseBO = FirstDemo.Infrastructure.BusinessObjects.Course;
+using CourseEO = FirstDemo.Infrastructure.Entities.Course;
 
 namespace FirstDemo.Infrastructure.Services
 {
-    public class CourseService
+    public class CourseService : ICourseService
     {
-        public void CreateCourse()
+        public void CreateCourse(CourseBO course)
         {
-
+            course.SetProperClassStartDate();
         }
     }
 }
