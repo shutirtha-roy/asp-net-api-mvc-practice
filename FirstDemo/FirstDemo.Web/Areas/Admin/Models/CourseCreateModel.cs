6f445aa7b3b33eed4fd7886990025f379a79d709
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-09-13 04:54:35 +0600

    CreateCourse Method Implemented

--- FirstDemo/FirstDemo.Web/Areas/Admin/Models/CourseCreateModel.cs
+++ FirstDemo/FirstDemo.Web/Areas/Admin/Models/CourseCreateModel.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using FirstDemo.Infrastructure.BusinessObjects;
+using FirstDemo.Infrastructure.Services;
+using System.ComponentModel.DataAnnotations;
 
 namespace FirstDemo.Web.Areas.Admin.Models
 {
@@ -8,11 +10,20 @@ namespace FirstDemo.Web.Areas.Admin.Models
         public string Title { get; set; }
         public double Fees { get; set; }
         public DateTime ClassStartDate { get; set; }
+        private readonly ICourseService _courseService;
+
+        public CourseCreateModel(ICourseService courseService)
+        {
+            _courseService = courseService;
+        }
 
         internal async Task CreateCourse()
         {
-            if(ClassStartDate == DateTime.MinValue)
-                ClassStartDate = DateTime.Now.AddDays(7);
+            Course course = new Course();
+            course.Name = Title;
+            course.Fees = Fees;
+            course.ClassStartDate = ClassStartDate;
+            _courseService.CreateCourse(course);
         }
     }
 }
