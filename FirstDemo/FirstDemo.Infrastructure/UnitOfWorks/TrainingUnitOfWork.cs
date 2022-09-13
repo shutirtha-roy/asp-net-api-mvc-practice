d93f64ebf38684ef1e1b08578475b786f69118d4
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-09-13 06:23:45 +0600

    TrainingUnitOfWork partially implemented

---
+++ FirstDemo/FirstDemo.Infrastructure/UnitOfWorks/TrainingUnitOfWork.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace FirstDemo.Infrastructure.UnitOfWorks
+{
+    public class TrainingUnitOfWork
+    {
+        private List<object> _itemsToAdd;
+        private List<object> _itemsToDelete;
+        private List<object> _itemsToModify;
+
+        public void Add(object item)
+        {
+            _itemsToAdd.Add(item);
+        }
+
+        public void Save()
+        {
+            using Transaction transaction;
+
+            try
+            {
+                foreach(var i in _itemsToAdd)
+                {
+                    // Find Repository
+                    // Add to Repository
+                }
+
+                foreach(var i in _itemsToDelete)
+                {
+
+                }
+
+                transaction.Commit();
+            }
+            catch (Exception ex)
+            {
+                transaction.Rollback();
+            }
+        }
+    }
+}
