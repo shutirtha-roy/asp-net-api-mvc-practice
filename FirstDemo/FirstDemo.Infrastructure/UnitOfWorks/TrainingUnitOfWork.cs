7b959b064da7d4b22e1a94be2367396e389ac3e3
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-09-13 06:26:54 +0600

    SqlTransaction corrected

--- FirstDemo/FirstDemo.Infrastructure/UnitOfWorks/TrainingUnitOfWork.cs
+++ FirstDemo/FirstDemo.Infrastructure/UnitOfWorks/TrainingUnitOfWork.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Data.SqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,7 +21,7 @@ namespace FirstDemo.Infrastructure.UnitOfWorks
 
         public void Save()
         {
-            using Transaction transaction;
+            using SqlTransaction transaction;
 
             try
             {
