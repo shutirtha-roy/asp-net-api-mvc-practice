1e51910b5931c93a9bcbb8c18c375702b7f0aaaa
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-09-20 17:18:51 +0600

    CourseListModel.cs Added

---
+++ FirstDemo/FirstDemo.Web/Areas/Admin/Models/CourseListModel.cs
@@ -0,0 +1,10 @@
+﻿namespace FirstDemo.Web.Areas.Admin.Models
+{
+    public class CourseListModel
+    {
+        internal object? GetPagedCourses(object dataTableModel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
