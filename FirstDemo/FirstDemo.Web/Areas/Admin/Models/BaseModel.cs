8b395a2805bbcf2f77827af83ada218cf14d68c8
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-09-21 17:27:31 +0600

    BaseModel Implemented

---
+++ FirstDemo/FirstDemo.Web/Areas/Admin/Models/BaseModel.cs
@@ -0,0 +1,14 @@
+﻿using Autofac;
+
+namespace FirstDemo.Web.Areas.Admin.Models
+{
+    public class BaseModel
+    {
+        protected ILifetimeScope _scope;
+
+        protected virtual void ResolveDependency(ILifetimeScope scope)
+        {
+            _scope = scope;
+        }
+    }
+}
