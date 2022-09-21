1ae83aaae5eff95a61feeb391565768830d2fa93
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-09-21 17:27:56 +0600

    Dependency Added

--- FirstDemo/FirstDemo.Web/Areas/Admin/Models/CourseListModel.cs
+++ FirstDemo/FirstDemo.Web/Areas/Admin/Models/CourseListModel.cs
@@ -1,7 +1,28 @@
-﻿namespace FirstDemo.Web.Areas.Admin.Models
+﻿using Autofac;
+using FirstDemo.Infrastructure.Services;
+
+namespace FirstDemo.Web.Areas.Admin.Models
 {
-    public class CourseListModel
+    public class CourseListModel : BaseModel
     {
+        private ICourseService? _courseService;
+
+        public CourseListModel() : base()
+        {
+
+        }
+
+        public CourseListModel(ICourseService courseService)
+        {
+            _courseService = courseService;
+        }
+
+        protected override void ResolveDependency(ILifetimeScope scope)
+        {
+            base.ResolveDependency(scope);
+            _courseService = _scope.Resolve<ICourseService>();
+        }
+
         internal object? GetPagedCourses(object dataTableModel)
         {
             throw new NotImplementedException();
