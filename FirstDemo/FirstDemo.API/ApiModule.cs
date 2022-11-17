c343e71df9edf219ea56c045bd0983675d400b72
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-11-18 01:10:43 +0530

    Serilog, Autofac and Database Configurations are added

---
+++ FirstDemo/FirstDemo.API/ApiModule.cs
@@ -0,0 +1,13 @@
+﻿using Autofac;
+
+namespace FirstDemo.Api
+{
+    public class ApiModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+
+            base.Load(builder);
+        }
+    }
+}
--- FirstDemo/FirstDemo.API/Program.cs
+++ FirstDemo/FirstDemo.API/Program.cs
@@ -1,25 +1,68 @@
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using FirstDemo.Api;
+using FirstDemo.Infrastructure;
+using FirstDemo.Infrastructure.DbContexts;
+using Microsoft.EntityFrameworkCore;
+using Serilog;
+using Serilog.Events;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Host.UseSerilog((ctx, lc) => lc
+    .MinimumLevel.Debug()
+    .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+    .Enrich.FromLogContext()
+    .ReadFrom.Configuration(builder.Configuration));
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+try
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    // Add services to the container.
+    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+    var assemblyName = Assembly.GetExecutingAssembly().FullName;
+
+    builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
+    builder.Host.ConfigureContainer<ContainerBuilder>(containerBuilder => {
+        containerBuilder.RegisterModule(new ApiModule());
+        containerBuilder.RegisterModule(new InfrastructureModule(connectionString,
+            assemblyName));
+    });
+
+    builder.Services.AddDbContext<ApplicationDbContext>(options =>
+        options.UseSqlServer(connectionString, m => m.MigrationsAssembly(assemblyName)));
+
+    builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+    builder.Services.AddControllers();
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
+    var app = builder.Build();
+
+    Log.Information("Application is starting");
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Application start-up failed");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
