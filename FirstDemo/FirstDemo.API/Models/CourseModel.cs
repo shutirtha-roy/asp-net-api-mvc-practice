bf3625181f08c4ae341069bd6003de77e09d15ed
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-12-06 20:49:13 +0600

    CourseModel Implemented

--- FirstDemo/FirstDemo.API/Models/CourseModel.cs
+++ FirstDemo/FirstDemo.API/Models/CourseModel.cs
@@ -1,5 +1,7 @@
 ﻿using Autofac;
 using AutoMapper;
+using FirstDemo.Infrastructure.BusinessObjects;
+using FirstDemo.Infrastructure.Models;
 using FirstDemo.Infrastructure.Services;
 
 namespace FirstDemo.API.Models
@@ -31,6 +33,63 @@ namespace FirstDemo.API.Models
             _mapper = scope.Resolve<IMapper>();
         }
 
+        internal IList<Course> GetCourses()
+        {
+            return _courseService?.GetCourses();
+        }
 
+        internal void DeleteCourse(Guid id)
+        {
+            _courseService?.DeleteCourse(id);
+        }
+
+        internal void CreateCourse()
+        {
+            Course course = _mapper.Map<Course>(this);
+
+            _courseService.CreateCourse(course);
+        }
+
+        internal void UpdateCourse()
+        {
+            Course course = _mapper.Map<Course>(this);
+
+            _courseService.EditCourse(course);
+        }
+
+        internal Course GetCourse(string name)
+        {
+            return _courseService.GetCourse(name);
+        }
+
+        internal Course GetCourse(Guid id)
+        {
+            return _courseService.GetCourse(id);
+        }
+
+        internal object? GetPagedCourses(DataTablesAjaxRequestModel model)
+        {
+
+            var data = _courseService?.GetCourses(
+                model.PageIndex,
+                model.PageSize,
+                model.SearchText,
+                model.GetSortText(new string[] { "Title", "Fees", "ClassStartDate" }));
+
+            return new
+            {
+                recordsTotal = data?.total,
+                recordsFiltered = data?.totalDisplay,
+                data = (from record in data?.records
+                        select new string[]
+                        {
+                                record.Name,
+                                record.Fees.ToString(),
+                                record.ClassStartDate.ToString(),
+                                record.Id.ToString()
+                        }
+                    ).ToArray()
+            };
+        }
     }
 }
