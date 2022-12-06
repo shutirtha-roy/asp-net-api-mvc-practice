91d9fdaf605b624cfb82a143b6af014271ae2dad
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-12-06 20:35:46 +0600

    CourseModel Added

---
+++ FirstDemo/FirstDemo.API/Models/CourseModel.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using FirstDemo.Infrastructure.Services;
+
+namespace FirstDemo.API.Models
+{
+    public class CourseModel
+    {
+        private ICourseService? _courseService;
+        private IMapper _mapper;
+
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public double Fees { get; set; }
+        public DateTime ClassStartDate { get; set; }
+
+        public CourseModel()
+        {
+
+        }
+
+        public CourseModel(ICourseService courseService, IMapper mapper)
+        {
+            _courseService = courseService;
+            _mapper = mapper;
+        }
+    }
+}
