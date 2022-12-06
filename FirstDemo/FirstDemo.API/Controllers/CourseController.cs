38c5e70b7025b74aa184566c71c746e8baa6a567
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-12-06 20:34:09 +0600

    CourseController Added

---
+++ FirstDemo/FirstDemo.API/Controllers/CourseController.cs
@@ -0,0 +1,22 @@
+﻿using Autofac;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstDemo.API.Controllers
+{
+    [Route("api/v3/[controller]")]
+    [ApiController]
+    public class CourseController : ControllerBase
+    {
+        private readonly ILifetimeScope _scope;
+        private readonly ILogger<CourseController> _logger;
+
+        public CourseController(ILogger<CourseController> logger, ILifetimeScope scope)
+        {
+            _logger = logger;
+            _scope = scope;
+        }
+
+
+    }
+}
