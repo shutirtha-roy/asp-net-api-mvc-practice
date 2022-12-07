cda15fb149fd225f60c53dd1dcc3e244f91e3d5d
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-12-07 18:16:10 +0600

    ApiRequirement and ApiRequirementHandler Implemented

---
+++ FirstDemo/FirstDemo.Infrastructure/Securities/ApiRequirementHandler.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace FirstDemo.Infrastructure.Securities
+{
+    public class ApiRequirementHandler :
+          AuthorizationHandler<ApiRequirement>
+    {
+        protected override Task HandleRequirementAsync(
+               AuthorizationHandlerContext context,
+               ApiRequirement requirement)
+        {
+            if (context.User.HasClaim(x => x.Type == "ViewCourse" && x.Value == "true"))
+            {
+                context.Succeed(requirement);
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
