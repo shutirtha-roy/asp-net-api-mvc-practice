bb38ddc2dcc4c1435ea6f769f30ecc47e9abf6e9
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-12-07 12:11:38 +0600

    ApiProfile Implemented

---
+++ FirstDemo/FirstDemo.API/Profiles/ApiProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using FirstDemo.API.Models;
+using FirstDemo.Infrastructure.BusinessObjects;
+
+namespace FirstDemo.API.Profiles
+{
+    public class ApiProfile : Profile
+    {
+        public ApiProfile()
+        {
+            CreateMap<CourseModel, Course>()
+                .ForMember(dest => dest.Name, src => src.MapFrom(x => x.Title))
+                .ReverseMap();
+        }
+    }
+}
