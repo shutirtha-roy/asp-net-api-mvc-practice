30cc646799fd45be3c31ea3a8eade7727b6c7372
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-12-21 20:22:15 +0600

    Calculator Unit Tests Implemented

--- Demo/DemoLib.Tests/CalculatorTests.cs
+++ Demo/DemoLib.Tests/CalculatorTests.cs
@@ -1,4 +1,6 @@
 using NUnit.Framework;
+using Shouldly;
+using System;
 
 namespace DemoLib.Tests
 {
@@ -21,6 +23,32 @@ namespace DemoLib.Tests
             int result = calculator.Sum(a, b);
 
             Assert.AreEqual(expected, result);
+        }
+
+        [Test, Category("UnitTest")]
+        public void Divide_ValidNumbers_ValidResult()
+        {
+            int a = 10;
+            int b = 2;
+            int expected = 5;
+
+            Calculator calculator = new Calculator();
+            int result = calculator.Divide(a, b);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test, Category("UnitTest")]
+        public void Divide_InvalidDivisor_ThrowException()
+        {
+            int a = 5;
+            int b = 0;
+
+            Calculator calculator = new Calculator();
+            Should.Throw<InvalidOperationException>(() =>
+            {
+                calculator.Divide(a, b);  
+            });
         }
     }
 }
--- Demo/DemoLib/Calculator.cs
+++ Demo/DemoLib/Calculator.cs
@@ -2,6 +2,14 @@
 {
     public class Calculator
     {
+        public int Divide(int a, int b)
+        {
+            if (b != 0)
+                return a / b;
+            else
+                throw new InvalidOperationException("Divisor can't be zero");
+        }
+
         public int Sum(int a, int b)
         {
             return a + b;
