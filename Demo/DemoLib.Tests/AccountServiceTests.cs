079df20052333b50cf3f7f8861494a394aed9c06
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-12-22 20:49:53 +0600

    Tested with Mock Objects

--- Demo/DemoLib.Tests/AccountServiceTests.cs
+++ Demo/DemoLib.Tests/AccountServiceTests.cs
@@ -1,12 +1,38 @@
-﻿using NUnit.Framework;
+﻿using Autofac.Extras.Moq;
+using Moq;
+using NUnit.Framework;
 
 namespace DemoLib.Tests
 {
     public class AccountServiceTests
     {
+        private AutoMock _mock;
+        private Mock<IEmailSender> _emailSender;
+        private AccountService _accountService;
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            _mock = AutoMock.GetLoose();
+        }
+
         [SetUp]
         public void Setup()
         {
+            _emailSender = _mock.Mock<IEmailSender>();
+            _accountService = _mock.Create<AccountService>();
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            _emailSender.Reset();
+        }
+
+        [OneTimeTearDown]
+        public void OneTimeTearDown()
+        {
+            _mock?.Dispose();
         }
 
         [Test, Category("UnitTest")]
@@ -16,8 +42,11 @@ namespace DemoLib.Tests
             string password = "12344";
             string email = "info@roy.com";
 
-            AccountService accountService = new AccountService(new FakeEmailSender());
-            accountService.CreateAccount(username, password, email);
+            _emailSender.Setup(x => x.Send(email)).Verifiable();
+
+            _accountService.CreateAccount(username, password, email);
+
+            _emailSender.VerifyAll();
         }
     }
 }
