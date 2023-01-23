c5c686eed34167bedc709b502fd09ac0f3a9dcde
Shutirtha Roy <shutirtha.roy1@gmail.com>
2023-01-23 13:41:00 +0600

    Prototype Design Pattern

---
+++ OOP/CreationalPattern/Hero.cs
@@ -0,0 +1,26 @@
+﻿namespace CreationalPattern
+{
+    public class Hero : ICloneable
+    {
+        public string Name { get; set; }
+        public string Color { get; set; }
+        public double Speed { get; set; }
+        public int Power { get; set; }
+
+        public object Clone()
+        {
+            return Copy();
+        }
+
+        public Hero Copy()
+        {
+            return new Hero
+            {
+                Name = Name,
+                Color = Color,
+                Speed = Speed,
+                Power = Power
+            };
+        }
+    }
+}
