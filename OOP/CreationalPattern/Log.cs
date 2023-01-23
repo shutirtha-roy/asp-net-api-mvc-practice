e98c2f44eac777e4a0a78d475bcf2ad6a61aa66b
Shutirtha Roy <shutirtha.roy1@gmail.com>
2023-01-23 13:21:59 +0600

    Singleton Pattern

---
+++ OOP/CreationalPattern/Log.cs
@@ -0,0 +1,32 @@
+﻿namespace CreationalPattern
+{
+    public class Log
+    {
+        private static Log _log;
+
+        private Log()
+        {
+
+        }
+
+        public static Log GetLogger()
+        {
+            //One threaded Singleton Pattern
+            if (_log == null)
+            {
+                _log = new Log();
+            }
+
+            //If Multi-threaded Singleton Pattern
+            //lock("lock")
+            //{
+            //    if (_log == null)
+            //    {
+            //        _log = new Log();
+            //    }
+            //}
+            
+            return _log;
+        }
+    }
+}
